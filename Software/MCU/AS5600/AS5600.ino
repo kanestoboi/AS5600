ff631898875c2e123b51d7cb9ae7a70441ffca20
Kane Stoboi <kanestoboi@gmail.com>
2018-09-30 08:39:51 +1000

    started implementing contro system in arduino code

--- Software/MCU/AS5600/AS5600.ino
+++ Software/MCU/AS5600/AS5600.ino
@@ -4,16 +4,28 @@ AS5600 encoder;
 int encoderStatus;
 long output = 0;
 int encoderPos;
+long setPoint = 2000;
+
+uint8_t stepPin = 3;
+uint8_t dirPin = 4;
+
+long startI = millis();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  digitalWrite(dirPin, HIGH);
+  //analogWrite(stepPin, 200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   encoderStatus = encoder.getStatus();
 
+  
   if (encoderStatus == 0b00001000)
     Serial.print("MH  ");
   else if (encoderStatus == 0b00010000)
@@ -23,9 +35,25 @@ void loop() {
   else
     Serial.print("Error  ");
   encoderPos = encoder.getPosition();
-  output = long(0.5335*(float)output + 0.4665*(float)encoderPos);
+  output = long(0.9332*(float)output + 0.06678*(float)encoderPos);
 
+  if (output < setPoint && abs(output - setPoint) > 50) {
+    digitalWrite(dirPin, HIGH);
+    takeStep();
+    takeStep();
+  }
+  else if (abs(output - setPoint) > 50) { 
+    digitalWrite(dirPin, LOW);
+    takeStep();
+    takeStep();
+  }
   //Serial.println(encoder.getPosition());
 
   Serial.println(output);
 }
+
+void takeStep() {
+  digitalWrite(stepPin, HIGH);
+  digitalWrite(stepPin, LOW); 
+}
+
