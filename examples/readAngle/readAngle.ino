8f77c4f5667bfc8ea6cb5401119c0ca533e81628
Kane Stoboi <kanestoboi@gmail.com>
2019-02-09 20:28:57 +1100

    added functionality to readAngle example

--- examples/readAngle/readAngle.ino
+++ examples/readAngle/readAngle.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <AS5600.h>
 
+AS5600 encoder;
+double output;
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  output = encoder.getPosition(); // get the absolute position of the encoder
+  Serial.println(output);
 }
