c4f2d7beacb97dc05699170182f8879b4dbbf12d
abetusk <abetusk@gmail.com>
2019-04-02 00:02:48 -0400

    many bug fixes:
    * _getRegister2 broken, fixed with call to _getRegister twice
    * _getRegister broken, fixed with adding extra parameter to `requestFrom`
    * changed types to explicit bit lenght (e.g. `uint16_t`)
    * added `isMagnet...` functions for convenience
    * added `getScaledAngle` for easy conversion to degrees (0 to 360)
    * took out buggy mamber variables `_msb` and `_lsb` variables
    * added example for angle
    * formatted code
    * tested on mocoder (AS5600 breakout)

--- AS5600.cpp
+++ AS5600.cpp
@@ -1,77 +1,84 @@
 #include "Arduino.h"
 #include "AS5600.h"
 
-AS5600::AS5600()
-{
-  //set up AS5600
+AS5600::AS5600() {
   Wire.begin();
-
-}
-long AS5600::getPosition()
-{
-  return _getRegisters2(_RAWANGLEAddressMSB, _RAWANGLEAddressLSB);  
 }
 
-int AS5600::getAngle()
-{
-  return _getRegisters2(_ANGLEAddressMSB, _ANGLEAddressLSB);  
+uint16_t AS5600::getPosition() {
+  return _getRegisters2(_RAWANGLEAddressMSB, _RAWANGLEAddressLSB);
 }
 
-int AS5600::getStatus()
-{
+uint16_t AS5600::getAngle() {
+  return _getRegisters2(_ANGLEAddressMSB, _ANGLEAddressLSB);
+}
+
+uint16_t AS5600::getRawAngle() {
+  return _getRegisters2(_RAWANGLEAddressMSB, _RAWANGLEAddressLSB);
+}
+
+float AS5600::getScaledAngle() {
+  int ang_hi = _getRegister(_RAWANGLEAddressMSB);
+  int ang_lo = _getRegister(_RAWANGLEAddressLSB);
+  return ang_hi * 22.5 + ang_lo * 0.087890625;
+}
+
+uint8_t AS5600::getStatus() {
   return _getRegister(_STATUSAddress) & 0b00111000;
 }
 
-int AS5600::getGain()
-{
+bool AS5600::isMagnetTooStrong() {
+  uint8_t _b=0;
+  _b = getStatus();
+  if (_b & (1<<5)) { return true; }
+  return false;
+}
+
+bool AS5600::isMagnetTooWeak() {
+  uint8_t _b=0;
+  _b = getStatus();
+  if (_b & (1<<4)) { return true; }
+  return false;
+}
+
+bool AS5600::isMagnetDetected() {
+  uint8_t _b=0;
+  _b = getStatus();
+  if (_b & (1<<3)) { return true; }
+  return false;
+}
+
+uint8_t AS5600::getGain() {
   return _getRegister(_AGCAddress);
 }
 
-int AS5600::getMagnitude()
-{
+uint8_t AS5600::getMagnet() {
+  return _getRegister(_AGCAddress);
+}
+
+uint16_t AS5600::getMagnitude() {
   return _getRegisters2(_MAGNITUDEAddressMSB, _MAGNITUDEAddressLSB);
 }
 
-int AS5600::_getRegister(byte register1)
-{
+uint8_t AS5600::_getRegister(byte register1) {
+  uint8_t _b=0;
+
   Wire.beginTransmission(_AS5600Address);
   Wire.write(register1);
   Wire.endTransmission();
 
-  Wire.requestFrom(_AS5600Address, 1);
+  Wire.requestFrom(_AS5600Address, 1, 0);
 
-  if(Wire.available() <=1) {
-    _msb = Wire.read();
-  }
+  while (Wire.available() == 0) { }
+  _b = Wire.read();
 
-  return _msb;
+  return _b;
 }
 
-long AS5600::_getRegisters2(byte registerMSB, byte registerLSB)
-{
-  _lsb = 0;
-  _msb = 0;
-  
-  Wire.beginTransmission(_AS5600Address);
-  Wire.write(registerMSB);
-  Wire.endTransmission();
-  delay(10);
+uint16_t AS5600::_getRegisters2(byte registerMSB, byte registerLSB) {
+  uint16_t _hi=0, _lo=0;
 
-  Wire.requestFrom(_AS5600Address, 1);
-
-  if(Wire.available() <=1) {
-    _msb = Wire.read();
-  }
-
-  Wire.requestFrom(_AS5600Address, 1);
-
-  Wire.beginTransmission(_AS5600Address);
-  Wire.write(registerLSB);
-  Wire.endTransmission();
-
-  if(Wire.available() <=1) {
-    _lsb = Wire.read();
-  }
-
-  return (_lsb) + (_msb & _msbMask) * 256;
+  _hi = _getRegister(registerMSB);
+  _lo = _getRegister(registerLSB);
+  return (_hi<<8) | (_lo);
 }
